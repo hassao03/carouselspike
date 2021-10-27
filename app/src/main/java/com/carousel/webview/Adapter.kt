4404c349e28a06fb33eff9eb4b1eb886db0e839a
hassao03 <omar.hassan@bbc.co.uk>
2021-10-27 14:38:22 +0100

    Make textview scrollable

--- app/src/main/java/com/carousel/webview/Adapter.kt
+++ app/src/main/java/com/carousel/webview/Adapter.kt
@@ -1,13 +1,15 @@
 package com.carousel.webview
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.webkit.WebView
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class Adapter (val rowItems: List<Rows>): RecyclerView.Adapter<RecyclerView.ViewHolder> (){
+class Adapter(val context: Context, val rowItems: List<Rows>): RecyclerView.Adapter<RecyclerView.ViewHolder> (){
 
     interface Rows
     class TextRow(val text: MutableList<String>): Rows
@@ -15,6 +17,7 @@ class Adapter (val rowItems: List<Rows>): RecyclerView.Adapter<RecyclerView.View
 
     class TextViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
         val textView: TextView = itemView.findViewById(R.id.text_view)
+        val imageView: ImageView = itemView.findViewById(R.id.image_view)
     }
 
     class WebViewViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
@@ -32,7 +35,7 @@ class Adapter (val rowItems: List<Rows>): RecyclerView.Adapter<RecyclerView.View
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
         when(viewType){
             TYPE_TEXT -> TextViewHolder(LayoutInflater.from(parent.context)
-                .inflate(R.layout.text_item, parent, false))
+                .inflate(R.layout.carousel_item, parent, false))
             TYPE_WEBVIEW -> WebViewViewHolder(LayoutInflater.from(parent.context)
                 .inflate(R.layout.webview_item, parent, false))
             else -> throw IllegalArgumentException()
--- app/src/main/java/com/carousel/webview/MainActivity.kt
+++ app/src/main/java/com/carousel/webview/MainActivity.kt
@@ -2,15 +2,18 @@ package com.carousel.webview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main_without_layout)
 
         val recyclerView: RecyclerView = findViewById(R.id.recycler_view)
-        recyclerView.adapter = Adapter(addItems())
+        recyclerView.adapter = Adapter(this, addItems())
+        recyclerView.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
     }
 
     private fun addItems(): List<Adapter.Rows>{
